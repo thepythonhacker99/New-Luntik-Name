4f9cd685c9ca3769f00f6fd0e5c02d2baa309138
thepythonhacker99 <sandy.chicu@gmail.com>
2023-08-16 18:57:39 +0300

    jesse

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+[ $1 = u ] && { 
+  [ -d libs/SFML ] && ( cd libs/SFML; git pull ) || git clone https://github.com/SFML/SFML.git libs/SFML
+  [ -d libs/spdlog ] && ( cd libs/spdlog; git pull ) || git clone https://github.com/gabime/spdlog.git libs/spdlog
+}
+
 cmake -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug .
 cd build
 make -j8
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./build.sh
+[ $1 = u ] && ./build.sh u || ./build.sh
 ./build/Luntik
